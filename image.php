4a8b5cce97fe68ad1e02b50cd002897c0dfbd629
volgasimsek <volgasimsek@gmail.com>
2017-12-11 21:09:45 +0300

    Update image.php

--- image.php
+++ image.php
@@ -6,11 +6,11 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 0, 154);
+  $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 154, 0);
+  $color = imagecolorallocate($im, 0, 255, 0);
 } else {
-  $color = imagecolorallocate($im, 0, 0, 154);
+  $color = imagecolorallocate($im, 0, 0, 255);
 }
 
 // Draw a filled rectangle
